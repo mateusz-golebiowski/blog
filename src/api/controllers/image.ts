0910a124c8166019b9a03ca4dade1831ae998cf4
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2021-12-22 19:23:03 +0100

    fix for images

--- src/api/controllers/image.ts
+++ src/api/controllers/image.ts
@@ -1,20 +1,21 @@
 import {dataStorageConfig} from '../../config/dataStorageConfig';
 import path from 'path';
+import {Request, Response} from "express";
 
-export const upload = (req, res) => {
+export const upload = (req: Request, res: Response) => {
     if (req.file !== undefined) {
 
         res.send({
             'success' : 1,
             'file': {
-                'url' : `http://192.168.100.7:4000/api/v1/image/${req.file.filename}`,
+                'url' : `http://192.168.100.7:4000/api/v1/image/${req.file.filename}`, // todo: remove hardocded address
             }
         });
     }
 
 };
 
-export const show = (req, res) => {
+export const show = (req: Request, res: Response) => {
     let storage;
     if (process.env.NODE_ENV === 'production') {
         storage = process.env.IMAGE_STORAGE;
