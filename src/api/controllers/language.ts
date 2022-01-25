f092737ac107b5ce605b59d6334b2938f80a3c62
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2022-01-25 20:29:51 +0100

    roles

--- src/api/controllers/language.ts
+++ src/api/controllers/language.ts
@@ -35,66 +35,79 @@ export const getAllLanguages = async (req: Request, res: Response) => {
 };
 
 export const addLanguage = async (req: Request, res: Response) => {
-    const connection = DatabaseManager.getInstance().getConnection();
-    const languageRep = connection.getRepository(Language);
-    const newLanguage = new Language();
-    newLanguage.name = req.body.name;
-    newLanguage.code = req.body.code;
-    const result = await languageRep.save(newLanguage);
+    //@ts-ignore
+    const role = Number.parseInt(req.user.decoded.role.id)
+    if (role === 1) {
+        const connection = DatabaseManager.getInstance().getConnection();
+        const languageRep = connection.getRepository(Language);
+        const newLanguage = new Language();
+        newLanguage.name = req.body.name;
+        newLanguage.code = req.body.code;
+        const result = await languageRep.save(newLanguage);
 
-    res.send(result)
-};
-
-export const deleteLanguage = async (req: Request, res: Response) => {
-    const id = Number.parseInt(req.params.id)
-
-    const connection = DatabaseManager.getInstance().getConnection();
-    const languageRep = connection.getRepository(Language);
-    const foundLanguage = await languageRep.findOne({where: {id: id},
-        relations: ['articles', 'categories', 'articles.comments', 'articles.categories']
-    })
-    const articleRep = connection.getRepository(Article);
-    const commentRep = connection.getRepository(Comment)
-
-    if (foundLanguage) {
-        console.log(foundLanguage)
-        for (const art of foundLanguage.articles) {
-            const comments = new Comment();
-            comments.article = art;
-            await commentRep.delete(comments)
-            art.categories = [];
-            art.comments = [];
-            await articleRep.save(art);
-            await articleRep.delete([art.id]);
-        }
-        foundLanguage.articles = [];
-        foundLanguage.categories = [];
-        await languageRep.save(foundLanguage)
-        const result = await languageRep.delete([foundLanguage.id])
         res.send(result)
     }
 
 };
 
-export const editLanguage = async (req: Request, res: Response) => {
-    const id= Number.parseInt(req.params.id);
+export const deleteLanguage = async (req: Request, res: Response) => {
+    //@ts-ignore
+    const role = Number.parseInt(req.user.decoded.role.id)
+    if (role === 1) {
+        const id = Number.parseInt(req.params.id)
 
-    const connection = DatabaseManager.getInstance().getConnection();
-    const langRep = connection.getRepository(Language);
-    const result = await langRep.findOne({
-        where:{id: id}
-    });
-    if (result) {
-        result.name = req.body.name;
-        result.code = req.body.code;
-        await langRep.save(result)
+        const connection = DatabaseManager.getInstance().getConnection();
+        const languageRep = connection.getRepository(Language);
+        const foundLanguage = await languageRep.findOne({where: {id: id},
+            relations: ['articles', 'categories', 'articles.comments', 'articles.categories']
+        })
+        const articleRep = connection.getRepository(Article);
+        const commentRep = connection.getRepository(Comment)
+
+        if (foundLanguage) {
+            console.log(foundLanguage)
+            for (const art of foundLanguage.articles) {
+                const comments = new Comment();
+                comments.article = art;
+                await commentRep.delete(comments)
+                art.categories = [];
+                art.comments = [];
+                await articleRep.save(art);
+                await articleRep.delete([art.id]);
+            }
+            foundLanguage.articles = [];
+            foundLanguage.categories = [];
+            await languageRep.save(foundLanguage)
+            const result = await languageRep.delete([foundLanguage.id])
+            res.send(result)
+        }
+    }
+};
+
+export const editLanguage = async (req: Request, res: Response) => {
+    //@ts-ignore
+    const role = Number.parseInt(req.user.decoded.role.id)
+    if (role === 1) {
+        const id= Number.parseInt(req.params.id);
+
+        const connection = DatabaseManager.getInstance().getConnection();
+        const langRep = connection.getRepository(Language);
+        const result = await langRep.findOne({
+            where:{id: id}
+        });
+        if (result) {
+            result.name = req.body.name;
+            result.code = req.body.code;
+            await langRep.save(result)
+        }
+
+        const response: any = {};
+        response.data = {
+            ...result,
+        };
+        response.success = true;
+        res.send(response);
     }
 
-    const response: any = {};
-    response.data = {
-        ...result,
-    };
-    response.success = true;
-    res.send(response);
 };
 
